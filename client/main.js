e2841db9b6c7473236ab971aede81481a1436600
elessar0x3 <elessar0x3@aydindril.(none)>
2012-12-31 15:24:58 -0500

    adding clear button, put logging elements in template

--- client/main.js
+++ client/main.js
@@ -1,13 +1,26 @@
+Template.log.messages = function () {
+  return Messages.find();
+};
+
+Template.log.events(
+  {
+    'click .append_msg': function (event, template) {
+      Messages.insert({
+          text: $(template.find('.input_text'))
+            .val()
+      });
+    },
+  
+    'click .clear_msg': function (event, template) {
+      Messages.remove({});
+    }
+  }
+);
+
 Meteor.startup(function () {
 
-  $('#button1').on('click',function () {
-
-      console.log("Click function");
-      var retval = Messages.insert({text: $('#input').val()});
-      // alert(retval);
-  });
-
 });
+
 
 
 
--- models/messages.js
+++ models/messages.js
@@ -2,3 +2,5 @@
 
 var Messages = new Meteor.Collection("messages");
 
+
+
