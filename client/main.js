7843caa4f9d7c0483f0c9a0c5d19514fe1381435
elessar0x3 <elessar0x3@aydindril.(none)>
2012-12-30 19:33:28 -0500

    adding message

--- client/main.js
+++ client/main.js
@@ -1,2 +1,13 @@
 Meteor.startup(function () {
+
+  $('#button1').on('click',function () {
+
+      console.log("Click function");
+      var retval = Messages.insert({text: $('#input').val()});
+      // alert(retval);
+  });
+
 });
+
+
+
--- server/main.js
+++ server/main.js
@@ -1,2 +1,5 @@
 Meteor.startup(function () {
+
+//var Messages = new Meteor.Collection("messages");
+
 });
