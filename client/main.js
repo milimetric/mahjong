fe7a199bd07ac0e94e6c140fd16152b99d920c0a
milimetric <dandreescu@wikimedia.org>
2013-01-11 22:28:31 -0500

    fixed template to be re-usable

--- client/main.js
+++ client/main.js
@@ -1,57 +1,51 @@
-global_temp = [];
-Template.log.created = function () {
-  var log_id = Meteor.uuid();
-  // alert(log_id);
-  // Create a new collection object to hold messages
-  this.text_log = new Meteor.Collection(log_id);
-
-
-  // Make the game controller know about this new collection
-  // Maybe put a general cursor of the local collection into the game controller
-}
-
-Template.log.messages = function (options) {
-// This function is called each time the template is instantiated
-// Return different collection cursors depending on what is doing the calling
-//AAAAHHHHH i cant figure this out - how do i decide which one of the logs is
-// calling it?
-  return Messages.find();
+Template.loggers.one = function () {
+    return { label: 'one' };
 };
 
-Template.log.events(
-  {
+Template.loggers.two = function () {
+    return { label: 'two' };
+};
+
+Template.log.created = function (options) {
+    // Make the game controller know about this new collection
+    // Maybe put a general cursor of the local collection into the game controller
+    console.log(this.label);
+};
+
+Template.log.messages = function (options) {
+    // This function is called each time the template is instantiated
+    // Return different collection cursors depending on what is doing the calling
+    //AAAAHHHHH i cant figure this out - how do i decide which one of the logs is
+    // calling it?
+    return Messages.find({label: this.label});
+};
+
+Template.log.events({
     'click .append_msg': function (event, template) {
-      template.text_log.insert({
-          text: $(template.find('.input_text'))
-            .val()
-          });
-      Messages.insert({
-          text: $(template.find('.input_text'))
-            .val()
-      });
+        var message = $(template.find('.input_text'));
+        if (message.val() == null || message.val() == '') { return; }
+        Messages.insert({
+            text: message.val(),
+            label: this.label
+        });
+        message.val('');
     },
-  
+
     'click .clear_msg': function (event, template) {
-      template.text_log.remove({});
-      Message.remove({});
+        Messages.remove({label: this.label});
     }
-  }
-);
+});
 
 Meteor.startup(function () {
 
-  var name = "Taylor";
-  // var name = prompt("Name?", "Taylor");
+    var name = "Taylor";
+    // var name = prompt("Name?", "Taylor");
 
-  var player_id = Players.insert({"name": name, idle: true});
-  // alert(player_id);
-  Session.set("player_id", player_id);
+    var player_id = Players.insert({"name": name, idle: true});
+    // alert(player_id);
+    Session.set("player_id", player_id);
 
-  // Find an empty text box to join
-  // If no empty ones then dont join any
+    // Find an empty text box to join
+    // If no empty ones then dont join any
 
 });
-
-
-
-
